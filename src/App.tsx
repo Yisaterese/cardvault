cc18001c77ed2cbb6c75b69dcce3fb5185ecad3b
Yisaterese <teresejosephyisa@gmail.com>
2025-01-11 23:28:40 +0100

    first commit empty project

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+import React from 'react';
+
+export default function App() {
+    return (
+        <h1 className="text-3xl bg-green-300 font-bold text-red-700 underline" >
+            Hello world!
+        </h1>
+    );
+}
